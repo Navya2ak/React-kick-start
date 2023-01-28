a5e20e9e726306c69949cc5cb9fb6de6ea4cbfa1
navya <navya@metrictreelabs.com>
2023-01-28 13:29:32 +0530

    addded image from public folder to display

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
 function Test(props)
 {
     return (
-        <h1>
+       <>
+         <h1>
             Hey {props.name},This is my functional component
-        </h1>   
+        </h1>  
+        <p>There was a famous picture about a lady  </p> 
+        <img src={process.env.PUBLIC_URL+"./lady.png"} alt="lady"></img>
+       </> 
          )
 }
 export default Test
